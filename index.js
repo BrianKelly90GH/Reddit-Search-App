6be47e3637d4eb696422e9117bd9af87f4396dc3
Brian Kelly <briankelly90united@gmail.com>
2018-03-26 17:39:40 +0100

    added dynamic card titles from return results of api fetch. Works

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ searchForm.addEventListener('submit', e => {
         <div class="card">
           <img class="card-img-top" src="..." alt="Card image cap">
           <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">${post.title}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
