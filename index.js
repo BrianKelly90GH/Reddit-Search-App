71f4edb6602c8a975b5afe2f844a5e0b51bdc5d4
Brian Kelly <briankelly90united@gmail.com>
2018-03-26 16:10:51 +0100

    added event listener to search form and logs for testing the input fields

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert('Finddit it says hello');
+const searchForm = document.getElementById('search-form');
+const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', e => {
+    // Get search field value
+    const searchTermValue = searchInput.value;
+    console.log(searchTermValue);
+
+    // Get sort by checkbox value
+    const sortByValue = document.querySelector('input[name="sortby"]:checked').value;
+    console.log(sortByValue);
+
+    // Get limit field value
+    const searchLimitValue = document.getElementById('limit').value;
+    console.log(searchLimitValue);
+
+    // Check for valid input
+    if (searchTermValue == '') {
+      // Show message
+      showMessage('Please add a search term', 'alert-danger');
+    }
+    e.preventDefault();
+});
