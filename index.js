256a3a6182258f1009f20759f2bc73b9d9cafe21
Brian Kelly <briankelly90united@gmail.com>
2018-03-26 17:35:23 +0100

    added test for creating bootstap cards with for each. Works

--- index.js
+++ index.js
@@ -26,7 +26,22 @@ searchForm.addEventListener('submit', e => {
 
     // Search reddit
     reddit.search(searchTermValue, searchLimitValue, sortByValue).then(searchResults =>{
-      console.log(searchResults);
+      let output = '<div class="card-columns">';
+      // Loop through posts and test creation of div cards with bootstrap
+      searchResults.forEach(post => {
+        output += `
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        `;
+      })
+      output += '</div>';
+      document.getElementById('results').innerHTML = output;
     });
 
 
