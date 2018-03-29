c33eefa68a46607d9c97364cec4dd3749e162fa6
Brian Kelly <briankelly90united@gmail.com>
2018-03-29 17:25:14 +0100

    added comments and clean up the function in this file

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,12 +1,19 @@
+// Import fuction from other javascript file that fecthes data from the Reddit API
 import reddit from '../Javascript/searchRedditAPI';
 
+// Get the search form element
 const searchForm = document.getElementById('search-form');
+
+// Get the search input element
 const searchInput = document.getElementById('search-input');
+
+// Set a guard for the alert messages for empty input box
+let alertMessage = false;
 
 // Form event listener
 searchForm.addEventListener('submit', e => {
     // Get search field value
-    const searchTermValue = searchInput.value;
+    const searchTermValue = searchInput.value
 
     // Get sort by checkbox value
     const sortByValue = document.querySelector('input[name="sortby"]:checked').value;
@@ -15,8 +22,10 @@ searchForm.addEventListener('submit', e => {
     const searchLimitValue = document.getElementById('limit').value;
 
     // Check for valid input
-    if (searchTermValue == '') {
-      // Show message
+    if (searchTermValue == '' && alertMessage != true) {
+
+      // Set the guard of the alert message to true and show message
+      alertMessage = true;
       showMessage('Please add a search term', 'alert-danger');
     }
 
@@ -25,13 +34,18 @@ searchForm.addEventListener('submit', e => {
 
     // Search reddit
     reddit.search(searchTermValue, searchLimitValue, sortByValue).then(searchResults =>{
+
+      // Set output varible to html markup
       let output = '<div class="card-columns">';
       // Loop through posts and test creation of div cards with bootstrap
       searchResults.forEach(post => {
-        // Check for an image with the post
+
+        // Check for an image with the post and if doesnt set image to stock image
         let image = post.preview
         ? post.preview.images[0].source.url
         : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+
+        // Concat the results of the fetch to the output
         output += `
         <div class="card text-white bg-secondary mb-3">
           <img class="card-img-top" src="${image}" alt="Card image cap">
@@ -47,6 +61,7 @@ searchForm.addEventListener('submit', e => {
         </div>
         `;
       })
+      // Concat the end of the HTML tag to the output 
       output += '</div>';
       document.getElementById('results').innerHTML = output;
     });
@@ -56,6 +71,7 @@ searchForm.addEventListener('submit', e => {
 
 // This function takes in a string message and a class name for to create a div and manipulate the DOM
 function showMessage(message, className){
+
   // Create div
   const divMessage = document.createElement('div');
 
@@ -75,7 +91,9 @@ function showMessage(message, className){
   searchContainer.insertBefore(divMessage, search);
 
   // Message time out
-  setTimeout( () => document.querySelector('.alert').remove(), 2000)
+  setTimeout( () => document.querySelector('.alert').remove(), 2000 );
+  //Message guard time out
+  setTimeout( () => alertMessage = false, 2000 );
 }
 
 // Truncate String Function
